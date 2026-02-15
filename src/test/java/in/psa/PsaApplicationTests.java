b029c42a75dff5b1a93f80e8ff91264451371b2c
akrockzz2003@gmail.com <akrockzz2003@gmail.com>
2026-02-15 18:02:16 +0530

    defectfix-3456

--- src/test/java/in/psa/PsaApplicationTests.java
+++ src/test/java/in/psa/PsaApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PsaApplicationTests {
+        int x=100;
+        String y="A";
 
 	@Test
 	void contextLoads() {
